8e0f4e60f5b3c44b64ad644661d8a11694902a21
fazi250 <114046687+fazi250@users.noreply.github.com>
2022-12-28 11:23:39 +0530

    changes to the blackgame project

--- index.js
+++ index.js
@@ -1,29 +1,67 @@
-let firstCard = 5
-let secondCard = 10
-let sum = firstCard + secondCard
+let sum = 0
+let cards = []
 let sumEl = document.getElementById("sum-el")
-let isAlive = true
+let isAlive = false
 let hasBlackJack = false
 let messageEl = document.getElementById("message-el")
 let message = ""
+let cardEl = document.getElementById("card-el")
 
-sumEl.textContent = "Sum: " +  sum
 
-
+function generateRandomNumber()
+{
+    let randomNumber = Math.floor(Math.random() * 13) + 1
+    if (randomNumber > 10)
+    {
+        return 10
+    }
+    else if (randomNumber === 1)
+    {
+        return 11
+    }
+    return randomNumber
+}
 function renderGame()
 {
     sumEl.textContent = "Sum: " +  sum
+    cardEl.textContent = "Cards: "
+    for(let i = 0 ; i < cards.length ; i++)
+    {
+        cardEl.textContent += cards[i] + " "
+    }
     if (sum < 21)
     {
-        message = "Do you want to draw a new card"
+        message = "Do you want to draw a new card?"
     }
     else if (sum === 21)
     {
-        message = "You have got the black jack"
+        message = "You have got the black jack press start to play again"
+        hasBlackJack = true
     }
     else
     {
-        message = "You are out of the game"
+        message = "You are out of the game press start to play again"
+        isAlive = false
     }
     messageEl.textContent = message
+}
+function startGame()
+{
+    isAlive = true
+    hasBlackJack = false
+    let firstCard = generateRandomNumber()
+    let secondCard = generateRandomNumber()
+    cards = [firstCard,secondCard]
+    sum = firstCard + secondCard
+    renderGame()
+}
+function newCard()
+{
+    if (isAlive === true && hasBlackJack === false)
+    {
+        let newCardNumber = generateRandomNumber()
+        sum += newCardNumber
+        cards.push(newCardNumber)
+        renderGame()
+    }
 }
